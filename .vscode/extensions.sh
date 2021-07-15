789bc7aac14dd9a776c4679cd0f421018372663e
Willow <willow.ch@protonmail.com>
2021-07-14 20:58:05 -0500

    Updated the extensions script

--- .vscode/extensions.sh
+++ .vscode/extensions.sh
@@ -6,7 +6,6 @@ else
 	echo "JQ is missing; please install it."
 	exit 1
 fi
-for ex in $(cat extensions.json | jq '.recommendations | join(" ")' | cut -d "\"" -f 2)
-do
-code --install-extension $ex
+for ex in $(cat $(dirname $0)/extensions.json | jq '.recommendations | join(" ")' | cut -d "\"" -f 2); do
+	code --install-extension $ex
 done
