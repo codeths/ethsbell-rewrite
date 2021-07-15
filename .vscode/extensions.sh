73511bd31d658c93882345afc3f5e44f9fabec38
Willow <willow.ch@protonmail.com>
2021-07-14 19:59:15 -0500

    Added script to install recommended extensions

---
+++ .vscode/extensions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script installs all recommended extensions.
+if which jq; then
+	echo "JQ found."
+else
+	echo "JQ is missing; please install it."
+	exit 1
+fi
+for ex in $(cat extensions.json | jq '.recommendations | join(" ")' | cut -d "\"" -f 2)
+do
+code --install-extension $ex
+done
