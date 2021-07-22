abc68fa76d1ed8ad19b4e92a7c2c61804fbab688
Willow Carlson-Huber <willow.ch@protonmail.com>
2021-07-22 08:51:35 -0500

    added an empty default schedule

--- frontend/global/helpers.js
+++ frontend/global/helpers.js
@@ -7,7 +7,7 @@ async function get(endpoint = '/api/v1/today/now/near') {
 		.catch(() => null);
 }
 
-const config = JSON.parse(localStorage.getItem('schedule'));
+const config = JSON.parse(localStorage.getItem('schedule')) || {};
 
 function replace_period(period) {
 	if (period.length > 0) {
