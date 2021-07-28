85a85962033cec62e989de5f872f3ac94bb0d842
Albert Portnoy <albert@asportnoy.com>
2021-07-28 10:28:58 -0500

    Make calendar work

--- frontend/schedule/calendar.js
+++ frontend/schedule/calendar.js
@@ -1,120 +1,65 @@
 let all_data;
 
-const calendarEl = document.getElementById('calendar');
-const container_height = calendarEl.clientHeight;
+const calendarEl = document.getElementById('event-wrapper');
 const preferred_event_min_width = 300;
 const min_visible_width = 50;
-const min_event_height = 50;
+const min_event_height = 30;
 const padding = 5;
-let pixels_per_minute;
-
-let viewport_seconds = 3600 * 4; // The number of seconds the viewport should show
-let viewport_offset = 0;
-const row_height = 50; // The height of a row
-const box_height = 40; // The height of a box
-const row_start = 10; // The height rows start at
-const text_height = 30; // The size of the font
-let has_resize_listener = false;
-let scroll_timeout;
-let last_scroll_touch_x;
-let scrolled_distance = 0;
+const pixels_per_minute = 1.5;
 
 /// Place period boxes for a list of periods.
-function place_boxes(data_unprocessed) {
+function place_boxes(data_unprocessed, date = current_date()) {
 	const data = replace_period(data_unprocessed).filter(v => v);
-	const startTime = data[0].start_timestamp;
-	const endTime = data[data.length - 1].end_timestamp;
-	const msInDay = endTime - startTime;
-	pixels_per_minute = container_height / (msInDay / 60);
+	const startDate = date_from_api(data[0].start, date);
+	const startTime = startDate.getTime() / 1000;
+	const endDate = date_from_api(data[data.length - 1].end, date)
+	const endTime = endDate.getTime() / 1000;
+	const sInDay = endTime - startTime;
+	let containerHeight = sInDay / 60 * pixels_per_minute;
+	let lastColHeight = {};
 
 	// Resolve rows so everything is mutually non-intersecting.
 	const events = [];
-	let offsets = {
-		"0": 0
-	};
-	let fullOffset = 0;
 
-	let num_cols = 1;
-
-	data.sort((a, b) => a.start_timestamp - b.start_timestamp);
-	data.forEach((period) => {
-		let offset = 0;
-
+	data.sort((a, b) => date_from_api(a.start, date) - date_from_api(b.start, date)).forEach((period, i) => {
 		// Set up variables
-		const start = period.start_timestamp;
-		const end = period.end_timestamp;
+		const start = date_from_api(period.start, date).getTime() / 1000;
+		const end = date_from_api(period.end, date).getTime() / 1000;
 		const duration = end - start;
+		let heightChange = 0;
 
 		let startPos = ((start - startTime) / 60) * pixels_per_minute;
-
-		let nextFullOffset = fullOffset;
-		events.filter(e => e.endPos - e.offset < startPos && !e.colspan).forEach(event => {
-			console.log(event.period.friendly_name);
-			console.log(events);
-			let colspan = 1;
-
-			while (events.filter(e => e.col == event.col && e.period.friendly_name !== event.period.friendly_name).some(e => [event.offsetPos, event.endPos].some(p => p >= e.startPos && p <= e.endPos))) {
-				event.col++;
-			}
-			num_cols = Math.max(num_cols, event.col + 1);
-
-			while (event.col + colspan < num_cols && !events.filter(e => e.col == event.col + colspan).some(e => [event.startPos, event.endPos].some(p => p >= e.startPos && p <= e.endPos))) {
-				colspan++;
-			}
-
-			event.colspan = colspan;
-			for (let i = event.col; i < event.col + colspan; i++) {
-				if (!offsets[i]) offsets[i] = fullOffset;
-				offsets[i] += event.offset;
-			}
-
-			if (event.offset > nextFullOffset) nextFullOffset = event.offset;
-		});
-
-		fullOffset = nextFullOffset;
-
-		let offsetPos = startPos + fullOffset;
-
 		let height = (duration / 60) * pixels_per_minute;
+		let endPos = startPos + height;
 		if (height < min_event_height) {
-			offset += min_event_height - height;
+			heightChange = (min_event_height - height);
 			height = min_event_height;
 		}
 
-		const endPos = startPos + height;
 		let col = 0;
 
-		while (events.filter(e => e.col == col).some(e => [offsetPos, endPos].some(p => p >= e.startPos && p <= e.endPos))) {
+		while (events.filter(e => e.col == col).some(e => startPos < e.endPos || endPos < e.startPos)) {
 			col++;
 		}
 
-		// console.log(period.friendly_name);
-		// console.log(offsets);
-		// console.log(fullOffset);
-		// console.log(startPos);
-
-		startPos += (offsets[col] ?? fullOffset);
-		// console.log(startPos);
-
-
-		num_cols = Math.max(num_cols, col + 1);
+		lastColHeight[col] = heightChange;
 
 		events.push({
 			startPos,
 			endPos,
 			height,
 			col,
-			period,
-			offset
+			period
 		});
 	});
 
-	console.log(events);
-
+	const num_cols = Math.max(...events.map(e => e.col)) + 1;
 	const colwidth = calendarEl.clientWidth / num_cols;
 	const percent = 1 / num_cols * 100;
 
 	calendarEl.innerHTML = '';
+	containerHeight += Math.max(...Object.values(lastColHeight));
+	calendarEl.style.height = `${containerHeight}px`;
 
 	events.forEach((event) => {
 		let colspan = 1;
@@ -123,36 +68,42 @@ function place_boxes(data_unprocessed) {
 			colspan++;
 		}
 
-		let widthOffset = padding * -2;
+		let widthOffset = event.col == 0 ? 0 : padding * -2;
 		if (colwidth * colspan < preferred_event_min_width) {
 			widthOffset = preferred_event_min_width - colwidth * colspan;
 			if (widthOffset > colwidth) widthOffset += colwidth - min_visible_width;
 		}
 
+		let leftOffset = widthOffset * event.col;
+
 		const el = document.createElement('div');
 		el.classList.add('event');
 
 		el.style.top = `${event.startPos}px`;
-		el.style.height = `${event.height - padding * 2}px`;
+		el.style.height = `${event.height}px`;
 
-		el.style.left = `calc(${percent * event.col}% ${widthOffset < 0 ? '+' : '-'} ${Math.abs(widthOffset)}px)`
+		el.style.left = `calc(${percent * event.col}% ${leftOffset < 0 ? '+' : '-'} ${Math.abs(leftOffset)}px)`
 		el.style.width = `calc(${percent * colspan}% ${widthOffset < 0 ? '-' : '+'} ${Math.abs(widthOffset)}px)`;
 
 		el.style.zIndex = num_cols - event.col + 1;
 
-		el.addEventListener('click', (e) => popup(e, event.period));
-		el.addEventListener('mouseenter', (e) => popup(e, event.period));
+		const childEl = document.createElement('div');
+		childEl.classList.add('event-child');
+
+		childEl.addEventListener('click', (e) => popup(e, event.period));
+		childEl.addEventListener('mouseenter', (e) => popup(e, event.period));
 
 		const nameEl = document.createElement('span');
 		nameEl.classList.add('event-name');
 		nameEl.innerText = event.period.friendly_name;
-		el.appendChild(nameEl);
+		childEl.appendChild(nameEl);
 
 		const timeEl = document.createElement('span');
 		timeEl.classList.add('event-time');
-		timeEl.innerText = `${human_time(event.period.start)} - ${human_time(event.period.end)}`
-		el.appendChild(timeEl);
+		timeEl.innerText = `${human_time(event.period.start)} - ${human_time(event.period.end)} (${human_time_left(event.period.end, event.period.start, true)})`
+		childEl.appendChild(timeEl);
 
+		el.appendChild(childEl);
 		calendarEl.appendChild(el);
 	});
 }
