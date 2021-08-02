24829306aff846d9e5c99cb1fba43c57a9a2d220
Albert Portnoy <albert@asportnoy.com>
2021-08-02 15:45:24 -0500

    Don't create for before/after school

--- frontend/global/progress.js
+++ frontend/global/progress.js
@@ -4,16 +4,23 @@ function update_progress(data) {
 		return;
 	}
 
+	data = data[1].filter(x => !['BeforeSchool', 'AfterSchool'].includes(x.kind));
+
 	progress_parent.innerHTML = '';
-	for (const period of data[1]) {
-		const progress = document.createElement('div');
-		progress.classList.add('progress_bar');
-		progress.id = `progress_bar_${period.friendly_name.split(' ').join('_')}`;
-		const length = period.end_timestamp - period.start_timestamp;
-		const now = (Date.now() / 1000) - period.start_timestamp;
-		const position = now / length;
-		progress.style.setProperty('--width', `${position * 100}%`);
-		progress.title = `${period.friendly_name} progress`;
-		progress_parent.append(progress);
+	if (data.length === 0) {
+		progress_parent.style.display = 'none';
+	} else {
+		progress_parent.style.display = 'block';
+		for (const period of data) {
+			const progress = document.createElement('div');
+			progress.classList.add('progress_bar');
+			progress.id = `progress_bar_${period.friendly_name.split(' ').join('_')}`;
+			const length = period.end_timestamp - period.start_timestamp;
+			const now = (current_date().getTime() / 1000) - period.start_timestamp;
+			const position = now / length;
+			progress.style.setProperty('--width', `${position * 100}%`);
+			progress.title = `${period.friendly_name} progress`;
+			progress_parent.append(progress);
+		}
 	}
 }
