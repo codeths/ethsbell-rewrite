9db4fedf898df0820a3cffaa75aca62518e24e1d
Albert Portnoy <albert@asportnoy.com>
2021-08-04 12:28:43 -0500

    Add circular progress bar on homepage

--- frontend/global/progress.js
+++ frontend/global/progress.js
@@ -25,4 +25,27 @@ function update_progress(data) {
 	}
 }
 
+function calculate_progress_percent(period) {
+	const length = (date_from_api(period.end) - date_from_api(period.start)) / 1000;
+	const now = (current_date().getTime() - date_from_api(period.start)) / 1000;
+	const position = now / length;
+	return position;
+}
+
+function update_progress_circular(period, element) {
+	const percent = calculate_progress_percent(period);
+
+	const circle = element.querySelector('.progress-ring__circle');
+
+	const radius = circle.r.baseVal.value;
+	const circumference = radius * 2 * Math.PI;
+
+	circle.style.strokeDasharray = `${circumference} ${circumference}`;
+	circle.style.strokeDashoffset = `${circumference}`;
+
+	const offset = circumference - percent * circumference;
+	circle.style.strokeDashoffset = offset;
+}
+
 window.update_progress = update_progress;
+window.update_progress_circular = update_progress_circular;
