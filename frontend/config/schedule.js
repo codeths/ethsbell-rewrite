43117f63ce70617d5ccd5805ea51e1df4e5a8f12
Albert Portnoy <albert@asportnoy.com>
2021-08-13 18:37:47 -0500

    Change example period name

--- frontend/config/schedule.js
+++ frontend/config/schedule.js
@@ -26,6 +26,10 @@ getel('cfg').addEventListener('change', () => {
 	getel('upload').disabled = getel('cfg').files.length !== 1;
 });
 
+getel('include_period_name').addEventListener('change', () => {
+	populate();
+});
+
 function getel(id) {
 	const selector = `#${id}`;
 	return document.querySelector(selector);
@@ -60,6 +64,8 @@ function populate() {
 		{name: 'Block 8', code: 'Block8'},
 	].map(x => `<tr><td>${x.name}</td><td><input data-for="name" data-period="${x.code}" value="${initial.schedule[x.code]?.name || ''}"}></td><td><input type="url" data-for="url" data-period="${x.code}" value="${initial.schedule[x.code]?.url || ''}"}></td></tr>`).join('\n');
 	getel('include_period_name').checked = initial.include_period_name || initial.include_period_name === undefined;
+	// Sometimes I just like to watch the world burn.
+	getel('example_name').innerHTML = `<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">${getel('include_period_name').checked ? 'Block 1 - Custom Name' : 'Custom Name'}</a>`;
 }
 
 populate();
