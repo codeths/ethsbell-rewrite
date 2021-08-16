6828a699d24960fd950ef17d380e9acc79b61142
Albert Portnoy <albert@asportnoy.com>
2021-08-16 13:16:29 -0500

    Catch if file not found

--- frontend/advisory.js
+++ frontend/advisory.js
@@ -1,6 +1,6 @@
 for (const element of document.querySelectorAll('.advisory')) {
 	(async () => {
-		element.innerHTML = await fetch('/advisory.html').then(response => response.ok ? response.text() : '');
+		element.innerHTML = await fetch('/advisory.html').then(response => response.ok ? response.text() : '').catch(() => '');
 	})();
 }
 
