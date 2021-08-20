9109b49cb58cfd0c36b560c509428fd19a19c7cd
Albert Portnoy <albert@asportnoy.com>
2021-08-20 09:33:58 -0500

    Fix redirect query string

--- frontend/redirect.js
+++ frontend/redirect.js
@@ -5,6 +5,9 @@ const their_host = refer.origin;
 if (our_host !== their_host && !window.location.search.includes('no_default')) {
 	try {
 		const cfg = JSON.parse(localStorage.getItem('schedule'));
-		window.location.replace(cfg.default_page || '/');
+		const page = cfg.default_page || '/';
+		if (!window.location.pathname !== page) {
+			window.location.href = `${page}${window.location.search}`;
+		}
 	} catch {}
 }
