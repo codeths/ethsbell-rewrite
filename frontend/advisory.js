99bcc25c39ba2646c21de9ac1c65cdf8e2ff69a5
Albert Portnoy <albert@albertp.dev>
2021-12-10 22:39:27 -0600

    Add close button & remember

--- frontend/advisory.js
+++ frontend/advisory.js
@@ -1,7 +1,37 @@
 for (const element of document.querySelectorAll('.advisory')) {
 	(async () => {
-		element.innerHTML = await fetch('/advisory.html')
+		let text = await fetch('/advisory.html')
 			.then(response => (response.ok ? response.text() : ''))
+			.then(text => text.trim())
 			.catch(() => '');
+
+		if (!text) {
+			return;
+		}
+
+		const firstLine = text.slice(0, text.indexOf('\n') + 1);
+		let id = null;
+		if (firstLine && firstLine.startsWith('#')) {
+			id = firstLine.slice(1).trim();
+			text = text.slice(text.indexOf('\n') + 1);
+		}
+
+		if (id && getCookie('advisory') === id) {
+			return;
+		}
+
+		element.innerHTML = text;
+
+		const closebtn = document.createElement('button');
+		closebtn.innerHTML = '&times;';
+		closebtn.classList.add('advisory-close');
+		closebtn.addEventListener('click', () => {
+			if (id) {
+				setCookie('advisory', id);
+			}
+
+			element.remove();
+		});
+		element.append(closebtn);
 	})();
 }
