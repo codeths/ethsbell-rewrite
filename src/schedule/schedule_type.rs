370f104ded7945207c0ddb9c66252bb33114597a
Sparrow <sparrowrat@proton.me>
2025-08-12 12:42:06 -0500

    feat: add initial files for api v2
    Co-authored-by: mswider <matthew.swider@outlook.com>

--- src/schedule/schedule_type.rs
+++ src/schedule/schedule_type.rs
@@ -103,6 +103,87 @@ impl ScheduleType {
 			)
 		}
 	}
+
+	/// Returns a tuple of a Vec<Period> of the previous Periods, a Vec<Period> of the current periods,
+	/// and a Vec<Period> of the next periods.
+	#[must_use]
+	pub fn at_time_v2(&self, time: NaiveTime) -> (Option<Period>, Vec<Period>, Option<Period>) {
+		if self.periods.is_empty() {
+			(None, vec![], None)
+		} else {
+			let mut before: Option<Period> = None;
+			let mut current: Vec<Period> = vec![];
+			let mut next: Option<Period> = None;
+			self.periods.iter().for_each(|period| {
+				if period.end <= time {
+					if period.kind != PeriodType::Passing {
+						match before.clone() {
+							Some(before_) if before_.end < period.end => {
+								before = Some(period.clone());
+							}
+							None => before = Some(period.clone()),
+							_ => {}
+						}
+					}
+				} else if period.start > time {
+					if period.kind != PeriodType::Passing {
+						match next.clone() {
+							Some(next_) if next_.start > period.start => {
+								next = Some(period.clone());
+							}
+							None => next = Some(period.clone()),
+							_ => {}
+						}
+					}
+				} else {
+					current.push(period.clone());
+				}
+			});
+			match (&before, &current, &next) {
+				(Some(before), v, Some(next)) if v.is_empty() => {
+					current = vec![Period {
+						friendly_name: "Passing Period".to_string(),
+						start: before.end,
+						end: next.start,
+						start_timestamp: 0,
+						end_timestamp: 0,
+						kind: PeriodType::Passing,
+					}];
+				}
+				(None, v, Some(next)) if v.is_empty() => {
+					current = vec![Period {
+						friendly_name: "Before School".to_string(),
+						start: NaiveTime::from_hms_opt(0, 0, 0).unwrap_or_default(),
+						end: next.start,
+						start_timestamp: 0,
+						end_timestamp: 0,
+						kind: PeriodType::BeforeSchool,
+					}];
+				}
+				(Some(before), v, None) if v.is_empty() => {
+					current = vec![Period {
+						friendly_name: "After School".to_string(),
+						start: before.end,
+						end: NaiveTime::from_hms_opt(23, 59, 59).unwrap_or_default(),
+						start_timestamp: 0,
+						end_timestamp: 0,
+						kind: PeriodType::AfterSchool,
+					}];
+				}
+				_ => {}
+			}
+			let now = Local::now();
+			(
+				before.map(|v| v.populate(now)),
+				current
+					.iter()
+					.map(|v| v.clone().populate(now))
+					.collect::<Vec<Period>>(),
+				next.map(|v| v.populate(now)),
+			)
+		}
+	}
+
 	/// Returns the first period of the schedule with the kind Class(_).
 	#[must_use]
 	pub fn first_class(&self) -> Option<Period> {
